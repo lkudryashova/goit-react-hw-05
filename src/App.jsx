c5e794bc91d0780d2a682f2397c0f9f7de578c98
Lesya Kudryashova <lkudryashova.work@gmail.com>
2025-03-03 20:21:11 +0100

    HW05 update

--- src/App.jsx
+++ src/App.jsx
@@ -1,80 +1,42 @@
-import ErrorMessage from "./components/ErrorMessage/ErrorMessage";
-import ImageGallery from "./components/ImageGallery/ImageGallery";
-import ImageModal from "./components/ImageModal/ImageModal";
-import Loader from "./components/Loader/Loader";
-import LoadMoreBtn from "./components/LoadMoreBtn/LoadMoreBtn";
-import SearchBar from "./components/SearchBar/SearchBar";
-import { useState, useEffect } from "react";
-import { Toaster, toast } from "react-hot-toast";
-import * as imagesService from "./services/api";
+import { Route, Routes } from "react-router-dom";
+import { lazy, Suspense } from "react";
+import Navigation from "./components/Navigation/Navigation";
+/*import HomePage from "./pages/HomePage/HomePage";
+import MoviesPage from "./pages/MoviesPage/MoviesPage";
+import NotFoundPage from "./pages/NotFoundPage/NotFoundPage";
+import MovieDetailsPage from "./pages/MovieDetailsPage/MovieDetailsPage";
+import MovieCast from "./components/MovieCast/MovieCast";
+import MovieReviews from "./components/MovieReviews/MovieReviews";*/
+
+const HomePage = lazy(() => import("./pages/HomePage/HomePage"));
+const MoviesPage = lazy(() => import("./pages/MoviesPage/MoviesPage"));
+const MovieDetailsPage = lazy(
+  () => import("./pages/MovieDetailsPage/MovieDetailsPage")
+);
+const NotFoundPage = lazy(() => import("./pages/NotFoundPage/NotFoundPage"));
+const MovieCast = lazy(() => import("./components/MovieCast/MovieCast"));
+const MovieReviews = lazy(
+  () => import("./components/MovieReviews/MovieReviews")
+);
 
 export default function App() {
-  const [images, setImages] = useState([]);
-  const [query, setQuery] = useState("");
-  const [page, setPage] = useState(1);
-  const [loading, setLoading] = useState(false);
-  const [error, setError] = useState(null);
-  const [showModal, setShowModal] = useState(false);
-  const [selectedImage, setSelectedImage] = useState(null);
-
-  useEffect(() => {
-    if (!query) return;
-    const getData = async () => {
-      setLoading(true);
-      try {
-        const response = await imagesService.fetchImages(query, page);
-        setImages((prev) => [...prev, ...response.results]);
-      } catch (error) {
-        setError(error.message);
-      } finally {
-        setLoading(false);
-      }
-    };
-    getData();
-  }, [query, page]);
-
-  const handleSearch = (newQuery) => {
-    if (newQuery === "") {
-      toast.error("Please enter a search term");
-      return;
-    }
-    setQuery(newQuery);
-    setImages([]);
-    setPage(1);
-    setError(null);
-  };
-
-  const handleLoadMore = () => {
-    setPage((prev) => prev + 1);
-  };
-
-  const openModal = (image) => {
-    setSelectedImage(image);
-    setShowModal(true);
-  };
-
-  const closeModal = () => {
-    setSelectedImage(null);
-    setShowModal(false);
-  };
-
   return (
-    <div className="container">
-      <SearchBar onSubmit={handleSearch} />
-      {error && <ErrorMessage message={error} />}
-      <ImageGallery images={images} openModal={openModal} />
-      {loading && (
-        <div className="loader-wrapper">
-          <Loader />
-        </div>
-      )}
-      {images.length > 0 && !loading && (
-        <LoadMoreBtn onClick={handleLoadMore} />
-      )}
-      {showModal && (
-        <ImageModal selectedImage={selectedImage} closeModal={closeModal} />
-      )}
-      <Toaster />
-    </div>
+    <main>
+      <Navigation />
+      <Suspense fallback={<h2>loading...</h2>}>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+
+          <Route path="/movies" element={<MoviesPage />} />
+
+          <Route path="/movies/:movieId" element={<MovieDetailsPage />}>
+            <Route path="cast" element={<MovieCast />} />
+            <Route path="reviews" element={<MovieReviews />} />
+          </Route>
+
+          <Route path="*" element={<NotFoundPage />} />
+        </Routes>
+      </Suspense>
+    </main>
   );
 }
